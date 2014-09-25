f4705ab93f8b010904176edc73e7fc73fbc2d7d8
Tommy Wang <tommy@gaixie.org>
2014-09-25 23:28:47 +0800

    javadoc 的master 分支也publish

--- .utility/publish-javadoc.sh
+++ .utility/publish-javadoc.sh
@@ -4,26 +4,31 @@
 # see http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/ for details
 
 if [ "$TRAVIS_REPO_SLUG" == "gaixie/sandbox" ] && \
-   [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
-   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
-   [ "$TRAVIS_BRANCH" == "develop" ]; then
+    [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
+    [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
-  echo -e "Publishing javadoc...\n"
+    echo -e "Publishing javadoc...\n"
 
-  cp -R build/docs/javadoc $HOME/javadoc-snapshot
+    cp -R build/docs/javadoc $HOME/javadoc-latest
 
-  cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/gaixie/sandbox gh-pages > /dev/null
+    cd $HOME
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/gaixie/sandbox gh-pages > /dev/null
 
-  cd gh-pages
-  git rm -rf ./core-api/snapshot/javadoc
-  cp -Rf $HOME/javadoc-snapshot ./core-api/snapshot/javadoc
-  git add -f .
-  git commit -m "Lastest snapshot javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+    cd gh-pages
 
-  echo -e "Published Javadoc to gh-pages.\n"
-  
+    if ["$TRAVIS_BRANCH" == "master" ]; then
+        git rm -rf ./core-api/latest/javadoc
+        cp -Rf $HOME/javadoc-latest ./core-api/latest/javadoc
+    else
+        git rm -rf ./core-api/snapshot/javadoc
+        cp -Rf $HOME/javadoc-latest ./core-api/snapshot/javadoc
+    fi
+
+    git add -f .
+    git commit -m "Lastest snapshot javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+    git push -fq origin gh-pages > /dev/null
+
+    echo -e "Published Javadoc to gh-pages.\n"
 fi
